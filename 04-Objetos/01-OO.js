c08b492ddc2a829b16fa2a605ad5571758e7cfd1
Yuri <yurijohn.developer@gmail.com>
2021-08-20 21:05:29 -0300

    obj

---
+++ 04-Objetos/01-OO.js
@@ -0,0 +1,21 @@
+// Código não executável!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// Orientação a Objeto
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+objeto.processamento()
+
+// Principios importantes:
+// 1. Abstração
+// 2. Encapsulamento
+// 3. Herança (prototype)
+// 4. Polimorfismo
